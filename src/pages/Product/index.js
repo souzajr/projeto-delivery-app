6d59b9ed5cfd9311e76a2316b503ebb627b04b4f
souzajr <contato@purpletech.com.br>
2020-06-01 23:25:25 -0300

    Add initial struct of product page

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import {
   Text,
   View,
@@ -6,12 +7,18 @@ import {
   KeyboardAvoidingView,
   Platform,
   CheckBox,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
 import Header from '../../components/Header/index';
 import styles from './styles';
 
 export default function Product() {
+  const [quantity, setQuantity] = useState(0);
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : null}
@@ -19,7 +26,14 @@ export default function Product() {
     >
       <Header />
       <ScrollView>
-        <Text>Olá</Text>
+        <View style={styles.innerContainer}>
+          <Text style={styles.productTitle}>Nome do produto</Text>
+        </View>
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.productDescription}>
+            Descrição descrição descrição descrição
+          </Text>
+        </View>
         <View style={styles.hrContainer}><View style={styles.hr} /></View>
         <View style={{}}>
           <View>
@@ -28,13 +42,77 @@ export default function Product() {
           </View>
           <View>
             <Text>Opcional</Text>
-
             <CheckBox
               style={{ color: 'red' }}
             />
           </View>
         </View>
+        <View style={styles.hrContainer}><View style={styles.hr} /></View>
+        <View style={styles.labelContainer}>
+          <Text style={styles.label}>Algum comentário?</Text>
+        </View>
+        <View style={styles.innerContainer}>
+          <TextInput
+            placeholder="Ex: sem vinagrete."
+            style={styles.input}
+            placeholderTextColor="#777"
+            autoCorrect={false}
+            autoCapitalize="none"
+            returnKeyType="go"
+            multiline
+            numberOfLines={4}
+            onChangeText={() => {}}
+          />
+        </View>
       </ScrollView>
+      <View style={styles.bottomContainer}>
+        <View style={{ ...styles.quantitySection, marginRight: 25 }}>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              let i = quantity;
+              if (i > 0) {
+                i--;
+                setQuantity(i--);
+              }
+            }}
+          >
+            <Icon
+              name="minus"
+              color="#CB3F3F"
+              size={25}
+            />
+          </TouchableWithoutFeedback>
+          <Text style={styles.quantityText}>
+            {quantity}
+          </Text>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              let i = quantity;
+              i++;
+              setQuantity(i);
+            }}
+          >
+            <Icon
+              name="plus"
+              color="#CB3F3F"
+              size={25}
+            />
+          </TouchableWithoutFeedback>
+        </View>
+        <View style={styles.buttonSection}>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={styles.addProductButton}
+          >
+            <Text style={styles.buttonText}>
+              R$10,00
+            </Text>
+            <Text style={styles.buttonText}>
+              Adicionar
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </KeyboardAvoidingView>
   );
 }
--- src/pages/Product/styles.js
+++ src/pages/Product/styles.js
@@ -5,6 +5,99 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  innerContainer: {
+    marginTop: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  hrContainer: {
+    marginTop: 15,
+    paddingHorizontal: 30,
+  },
+  productTitle: {
+    color: '#CB3F3F',
+    fontSize: 21,
+  },
+  descriptionContainer: {
+    marginTop: 15,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    paddingHorizontal: 30,
+  },
+  productDescription: {
+    color: '#777',
+    fontStyle: 'italic',
+  },
+  hr: {
+    borderBottomColor: 'rgba(112, 112, 112, .3)',
+    borderBottomWidth: 1,
+  },
+  labelContainer: {
+    marginTop: 15,
+    marginBottom: -8,
+    alignItems: 'flex-start',
+    paddingHorizontal: '7.5%',
+  },
+  label: {
+    fontWeight: 'bold',
+    color: '#777',
+  },
+  input: {
+    width: '85%',
+    height: 50,
+    fontSize: 21,
+    borderColor: '#777',
+    borderWidth: 1,
+    borderRadius: 4,
+    color: '#777',
+    padding: 10,
+    marginBottom: 15,
+  },
+  bottomContainer: {
+    borderTopWidth: 1,
+    borderColor: '#777',
+    height: 75,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+  },
+  quantitySection: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  buttonSection: {
+    flex: 2,
+  },
+  quantityText: {
+    color: '#CB3F3F',
+    fontWeight: 'bold',
+    fontSize: 21,
+  },
+  addProductButton: {
+    width: '100%',
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    borderColor: '#CB3F3F',
+    backgroundColor: '#CB3F3F',
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 15,
+  },
 });
 
 export default styles;
